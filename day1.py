d9633a9751e52b020ae1d4436bc66a9fe25fdbde
Martijn <martijnkortleve@gmail.com>
2020-12-02 16:47:20 +0100

    formatting

--- day1.py
+++ day1.py
@@ -1,12 +1,19 @@
-lines = list(map(int, open('data.in').readlines()))
+lines = list(map(int, open("data.in").readlines()))
+
+for num1 in lines:
+    if (2020 - num1) in lines:
+        print(num1 * (2020 - num1))
+        break
 
 # part 2
 for num1 in lines:
     for num2 in lines:
-        if 2020 - num2 - num1 in lines:
+        if (2020 - num2 - num1) in lines:
             print(num1 * num2 * (2020 - num1 - num2))
             break
 
 # short solutuions
 next(print(x * (2020 - x)) for x in lines if 2020 - x in lines)
-next(print(x * y * (2020 - x - y)) for x in lines for y in lines if 2020 - y - x in lines)
+next(
+    print(x * y * (2020 - x - y)) for x in lines for y in lines if 2020 - y - x in lines
+)
--- day2.py
+++ day2.py
@@ -1,22 +1,22 @@
 import re
 
-with open('data2.in', "r") as f:
+with open("data2.in", "r") as f:
     lines = f.readlines()
 
-i=0
-j=0
+i = 0
+j = 0
 for x in lines:
-    a, b, c, d = re.match('(\d+)-(\d+) (\w): (\w*)', x).groups()
-    
+    a, b, c, d = re.match(r"(\d+)-(\d+) (\w): (\w*)", x).groups()
+
     # part 1
     if int(a) <= d.count(c) <= int(b):
-        i+=1
-    
+        i += 1
+
     # part 2
     if (d[int(a) - 1] == c) != (d[int(b) - 1] == c):
-        j+=1
-    
-print("part 1: {}, part 2: {}".format(i,j))
+        j += 1
+
+print("part 1: {}, part 2: {}".format(i, j))
 
 # short solutions
 # Part One
