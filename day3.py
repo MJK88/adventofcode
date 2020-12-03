62a74012fa3e647f996f7ebbf1ab67d7781a4348
Martijn <martijnkortleve@gmail.com>
2020-12-03 11:02:52 +0100

    day 3

---
+++ day3.py
@@ -0,0 +1,21 @@
+from math import prod
+
+with open("data3.in", "r") as f:
+    lines = f.read().splitlines()
+
+count = 0
+for i, x in enumerate(lines):
+    if x[i * 3 % (len(x))] == "#":
+        count += 1
+
+print("number of trees: {}".format(count))
+
+count = []
+for a, b in zip([1, 3, 5, 7, 1], [1, 1, 1, 1, 2]):
+    num = 0
+    for i, x in enumerate(lines[::b]):
+        if x[i * a % len(x)] == "#":
+            num += 1
+    count.append(num)
+
+print("number of trees of all slopes: {}".format(prod(count)))
