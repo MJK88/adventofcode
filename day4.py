442424a335cb19c1d0710470d92e60364d3eb2e7
Martijn <martijnkortleve@gmail.com>
2020-12-04 13:51:54 +0100

    added word borders

--- day4.py
+++ day4.py
@@ -8,13 +8,13 @@ print(sum([x.count(":") == 8 or (x.count(":") == 7 and not "cid" in x) for x in 
 
 # part 2
 patterns = [
-    r"hcl:#[0-9a-f]{6}",
+    r"hcl:#[0-9a-f]{6}\b",
     r"pid:[0-9]{9}\b",
-    r"byr:(19[2-9][0-9]|200[0-2])",
-    r"ecl:(amb|blu|brn|gry|grn|hzl|oth)",
-    r"iyr:(201[0-9]|2020)",
-    r"eyr:(202[0-9]|2030)",
-    r"hgt:(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)",
+    r"byr:(19[2-9][0-9]|200[0-2])\b",
+    r"ecl:(amb|blu|brn|gry|grn|hzl|oth)\b",
+    r"iyr:(201[0-9]|2020)\b",
+    r"eyr:(202[0-9]|2030)\b",
+    r"hgt:(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)\b",
 ]
 
 count = 0
