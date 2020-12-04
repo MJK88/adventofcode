d8f53830fd93ac93f0d525ee8e80ec5ccdb00f2a
Martijn <martijnkortleve@gmail.com>
2020-12-04 13:44:08 +0100

    it works!

--- day4.py
+++ day4.py
@@ -8,13 +8,13 @@ print(sum([x.count(":") == 8 or (x.count(":") == 7 and not "cid" in x) for x in 
 
 # part 2
 patterns = [
-    "hcl:#[0-9a-f]{6}",
-    "pid:[0-9]{9}",
-    "byr:(19[2-9][0-9]|200[0-2])",
-    "ecl:(amb|blu|brn|gry|grn|hzl|oth)",
-    "iyr:(20[1][0-9]|2020)",
-    "eyr:(20[2][0-9]|2030)",
-    "hgt:(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)",
+    r"hcl:#[0-9a-f]{6}",
+    r"pid:[0-9]{9}\b",
+    r"byr:(19[2-9][0-9]|200[0-2])",
+    r"ecl:(amb|blu|brn|gry|grn|hzl|oth)",
+    r"iyr:(201[0-9]|2020)",
+    r"eyr:(202[0-9]|2030)",
+    r"hgt:(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)",
 ]
 
 count = 0
