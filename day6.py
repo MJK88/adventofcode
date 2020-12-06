6fdd7b86e98a8e3e52f9dfc7612fb71c44738021
Martijn <martijnkortleve@gmail.com>
2020-12-06 15:03:23 +0100

    short part 2

--- day6.py
+++ day6.py
@@ -1,14 +1,24 @@
 with open("data6.in", "r") as f:
-    lines = [x.replace("\n", " ") for x in f.read().split("\n\n")]
+    lines = [x.split("\n") for x in f.read().split("\n\n")]
 
 # part 1
-print(sum([len(set(x.replace(" ", ""))) for x in lines]))
+print(sum([len(set("".join(y))) for y in lines]))
 
 # part 2
 count = 0
-for x in lines:
-    x = x.split()
-    for y in x[0]:
-        if all(y in z for z in x[1:]):
+for group in lines:
+    for chars in group[0]:
+        if all(chars in rest for rest in group[1:]):
             count += 1
 print(count)
+
+# short part 2
+print(
+    sum(
+        [
+            all(chars in rest for rest in group[1:])
+            for group in lines
+            for chars in group[0]
+        ]
+    )
+)
