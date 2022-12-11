90f4e33674ff8431c31bb9a872380a2a4ce00934
Martijn <martijnkortleve@gmail.com>
2022-12-11 22:49:14 +0100

    2022 day 11 part 2

--- 2022/day11.py
+++ 2022/day11.py
@@ -11,8 +11,10 @@ monkeyInstructionsRegex = (
 worryLevels = [re.findall(regexWorryLevel, x)[0].split(", ") for x in lines]
 instructions = [re.findall(monkeyInstructionsRegex, x)[0] for x in lines]
 
+lcmPart2 = math.lcm(*[int(instruction[2]) for instruction in instructions])
+
 monkeyStats = {}
-for rounds in range(20):
+for rounds in range(10000):
     for i, monkey in enumerate(worryLevels):
         for old in monkey:
             if i not in monkeyStats:
@@ -24,14 +26,12 @@ for rounds in range(20):
                 new = int(eval(old + instructions[i][0] + old))
             else:
                 new = int(eval(old + instructions[i][0] + instructions[i][1]))
-            new = new // 3
-            # print(new)
+            # new = new // 3
+            new = new % lcmPart2
             if new % int(instructions[i][2]) == 0:
                 worryLevels[int(instructions[i][3])].append(str(new))
             else:
                 worryLevels[int(instructions[i][4])].append(str(new))
             worryLevels[i] = worryLevels[i][1:]
 
-print(worryLevels)
-print(monkeyStats)
 print(math.prod(sorted(set(monkeyStats.values()))[-2:]))
